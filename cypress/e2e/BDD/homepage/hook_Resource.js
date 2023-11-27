6c9aa1286cbda35a6b23395322aa08a06a951e01
pape malick ba <pmba@agiltoo.fr>
2023-11-27 12:01:01 +0100

    commit test cloud

--- cypress/e2e/BDD/homepage/hook_Resource.js
+++ cypress/e2e/BDD/homepage/hook_Resource.js
@@ -10,4 +10,5 @@ beforeEach(()=> {
     cy.visit('/');
     cy.viewport(1440, 900) //pour ajuster la taille de l'ecran depuis cypress
     //cy.get('.osano-cm-denyAll').click() // ignorer la politique de confidentialite
+    //
 })
