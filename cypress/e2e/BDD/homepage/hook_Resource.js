3204926eec52ee3f6d86e53157ff0f7105b98a7c
pape malick ba <pmba@agiltoo.fr>
2023-11-27 14:37:21 +0100

    test cypress cloud

--- cypress/e2e/BDD/homepage/hook_Resource.js
+++ cypress/e2e/BDD/homepage/hook_Resource.js
@@ -10,15 +10,5 @@ beforeEach(()=> {
     cy.visit('/');
     cy.viewport(1440, 900) //pour ajuster la taille de l'ecran depuis cypress
     //cy.get('.osano-cm-denyAll').click() // ignorer la politique de confidentialite
-    // Vérifier si le bouton est visible
-    cy.get('.osano-cm-denyAll').should('be.visible').then((button) => {
-        // Le bouton est visible, vous pouvez le cliquer
-        button.click();
-        
-        // Ajouter vos actions supplémentaires ici si nécessaire après avoir cliqué sur le bouton
-    }).catch(() => {
-        // Le bouton n'est pas visible, passer à l'étape suivante
-        cy.log('Le bouton n\'est pas visible, passer à l\'étape suivante');
-        // Ajouter vos actions pour l'étape suivante ici
-    });
+    
 })
