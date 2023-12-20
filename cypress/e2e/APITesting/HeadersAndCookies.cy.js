a125484eb52c5cd6b258c8b6c6f1a439c0fb5baa
pape malick ba <pmba@agiltoo.fr>
2023-12-20 10:19:29 +0100

    implementation QueryParams and HeadersAndCookies

---
+++ cypress/e2e/APITesting/HeadersAndCookies.cy.js
@@ -0,0 +1,70 @@
+
+describe("API testing", ()=>{
+
+    let authToken = null
+
+    before("creating access token",()=>{
+
+        cy.request({
+            method: 'POST',
+            url: 'https://simple-books-api.glitch.me/api-clients/',
+            headers: { 
+                'Content-Type': 'application/json'
+            },
+            body:{
+                clientName: 'ABC',
+                clientEmail: Math.random().toString(5).substring(2)+"@gmail.com"
+            }
+        }). then((response) =>{
+                authToken = response.body.accessToken  //on récupère le token dans la variable 'authToken'
+        })
+    })
+
+
+
+    it("creating new order",() =>{
+        cy.request({
+            method: 'POST',
+            url: 'https://simple-books-api.glitch.me/orders/',
+            headers: { 
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer '+ authToken
+            },
+            body:{
+                bookId: 1,
+                customerName: "Malick"
+            }
+        }). then((response) =>{
+                expect(response.status).to.eq(201)
+                expect(response.body.created).to.eq(true)
+        })
+    })
+
+
+
+    it("Fetching all orders",() =>{
+        cy.request({
+            method: 'GET',
+            url: 'https://simple-books-api.glitch.me/orders/',
+            headers: { 
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer '+ authToken
+            },
+            cookies:{
+                'cookieName': 'mycookie'  //je dois voir l'importance des cookies
+            }
+
+        }). then((response) =>{
+                expect(response.status).to.eq(200)
+                expect(response.body).has.length(1)
+        })
+    })
+
+
+
+
+
+})
+
+
+
